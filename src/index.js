0b61c5782052a1697db95a8ebb742e9eeaf23f28
thaisemanuele <thatitis@gmail.com>
2020-05-26 16:26:05 +0300

    initial setup
    Setting up a repo to use redux
    Creating the first page
    Adding folder structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import { store } from './redux/store';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>, 
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <h1>HOME PAGE</h1>
+    </div>
+);
+
+export default HomePage;
