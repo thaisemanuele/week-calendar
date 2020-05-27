b6a76175f5f31e8d81c87fb4087205ae9f3ee352
thaisemanuele <thatitis@gmail.com>
2020-05-27 09:34:44 +0300

    useEffect in HomePage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { connect } from "react-redux";
 
 import "./App.css";
 
-import { fetchHolidays } from "./store/actions/holidays";
-
 import HomePage from "./pages/homepage/homepage.component";
 
-class App extends Component {
-  componentDidMount() {
-    const { initHolidays } = this.props;
-    initHolidays();
-  }
-  render() {
-    return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-        </Switch>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </div>
+  );
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  initHolidays: () => dispatch(fetchHolidays("2020-06-02", "2020-06-27")),
-});
-
-export default connect(null, mapDispatchToProps)(App);
+export default App;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 
 import "./homepage.styles.scss";
 import CalendarWeek from "../../components/calendar-week/calendarWeek.component";
 
-const HomePage = () => (
-  <div className="homepage">
-    <CalendarWeek />
-  </div>
-);
+import { fetchHolidays } from "../../store/actions/holidays";
 
-export default HomePage;
+const HomePage = (props) => {
+  useEffect(() => {
+    const { initHolidays } = props;
+    initHolidays();
+  }, [props]);
+
+  return (
+    <div className="homepage">
+      <CalendarWeek />
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  initHolidays: () => dispatch(fetchHolidays("2020-06-02", "2020-06-27")),
+});
+
+export default connect(null, mapDispatchToProps)(HomePage);
