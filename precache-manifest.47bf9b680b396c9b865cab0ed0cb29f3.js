4ccc02345cc017cc4f1dbe96c4a86422606f238d
thaisemanuele <thatitis@gmail.com>
2020-05-29 09:31:35 +0300

    Updates

---
+++ precache-manifest.47bf9b680b396c9b865cab0ed0cb29f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f049db2cfbf373efb75b317afa216d8",
+    "url": "/thaisemanuele/week-calendar/index.html"
+  },
+  {
+    "revision": "0f8500a640141d61156b",
+    "url": "/thaisemanuele/week-calendar/static/css/main.8494ed72.chunk.css"
+  },
+  {
+    "revision": "74fa7595ca693781d30a",
+    "url": "/thaisemanuele/week-calendar/static/js/2.f789b9ea.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/thaisemanuele/week-calendar/static/js/2.f789b9ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f8500a640141d61156b",
+    "url": "/thaisemanuele/week-calendar/static/js/main.684f948f.chunk.js"
+  },
+  {
+    "revision": "824692fe4eaca9f1ea0c",
+    "url": "/thaisemanuele/week-calendar/static/js/runtime-main.9ff95e67.js"
+  }
+]);
